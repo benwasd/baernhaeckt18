6b4913f87db2797d47861340ae339f2e5717e38b
Adrian Tschirren <at@nexplore.ch>
2018-08-25 19:21:43 +0200

    Made search more reliable

--- StiftungFileImporter/Zefix/ZefixSrv.cs
+++ StiftungFileImporter/Zefix/ZefixSrv.cs
@@ -10,7 +10,8 @@ namespace Zefix
             client.ClientCredentials.UserName.UserName = "UserName";
             client.ClientCredentials.UserName.Password = "Password";
 
-            var response = client.SearchByName(new searchByNameRequest {name = name});
+            var companyName = GetNameForSearch(name);
+            var response = client.SearchByName(new searchByNameRequest {name = companyName });
             var result = response.Item as shortResponseResult;
             if (result?.companyInfo == null || result.companyInfo.Length <= 0)
             {
@@ -26,6 +27,25 @@ namespace Zefix
                 Uid = company.uid,
                 ChId = company.chid
             };
+        }
+
+        private static string GetNameForSearch(string stiftungName)
+        {
+            var name = stiftungName;
+            if (name == null)
+            {
+                return string.Empty;
+            }
+
+            name = name.Replace("<(>&<)>", "*");
+
+            var bracketPosition = name.IndexOf('(');
+            if (bracketPosition > 1)
+            {
+                name = name.Substring(0, bracketPosition);
+            }
+
+            return name;
         }
     }
 }
