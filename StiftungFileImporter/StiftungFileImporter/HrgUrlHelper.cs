963b9591e719ee76a5475ee05bef8ba76dbbbdc4
Adrian Tschirren <at@nexplore.ch>
2018-08-25 17:00:35 +0200

    Map to matching canton registry

--- StiftungFileImporter/StiftungFileImporter/HrgUrlHelper.cs
+++ StiftungFileImporter/StiftungFileImporter/HrgUrlHelper.cs
@@ -1,10 +1,42 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using Zefix;
 
 namespace StiftungFileImporter
 {
     public class HrgUrlHelper
     {
+        private static IDictionary<string, string> mapping = new Dictionary<string, string>
+        {
+            {"20", "ZH"},
+            {"36", "BE"},
+            {"100", "LU"},
+            {"120", "UR"},
+            {"130", "SZ"},
+            {"140", "OW"},
+            {"150", "NW"},
+            {"160", "GL"},
+            {"170", "ZG"},
+            {"217", "FR"},
+            {"241", "SO"},
+            {"270", "BS"},
+            {"280", "BL"},
+            {"290", "SH"},
+            {"300", "AR"},
+            {"310", "AI"},
+            {"320", "SG"},
+            {"350", "GR"},
+            {"400", "AG"},
+            {"440", "TG"},
+            {"501", "TI"},
+            {"550", "VD"},
+            {"600", "VS"},
+            {"621", "VS"},
+            {"626", "VS"},
+            {"645", "NE"},
+            {"660", "GE"},
+            {"670", "JU"}
+        };
         public static string GetQueryUrl(CompanyInfo companyInfo)
         {
             var canton = GetCantonForLegalSeatId(companyInfo.RegisterOfficeId);
@@ -26,7 +58,8 @@ namespace StiftungFileImporter
 
         private static string GetCantonForLegalSeatId(string registerOfficeId)
         {
-            return "BE".ToLower();
+            var canton = mapping.ContainsKey(registerOfficeId) ? mapping[registerOfficeId] : "be";
+            return canton.ToLower();
         }
     }
 }
