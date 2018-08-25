19b878db3003293649ad90e43be5aeaaa42cf0c4
Adrian Tschirren <at@nexplore.ch>
2018-08-25 14:18:21 +0200

    initial

---
+++ StiftungFileImporter/StiftungFileImporter/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Mime;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AngleSharp;
+using Zefix;
+
+namespace StiftungFileImporter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var address = "https://be.chregister.ch/cr-portal/auszug/zefix.xhtml?uid=110389869&lang=de";
+            var address = "https://be.chregister.ch/cr-portal/auszug/auszug.xhtml?uid=CHE-105.830.305";
+
+            
+
+            //var client = new HttpClient();
+            //var response = client.GetAsync(address).Result;
+
+            //if (response.StatusCode == HttpStatusCode.OK)
+            //{
+            //    var body = response.Content.ReadAsStringAsync().Result;
+
+            //}
+
+
+            // AngleSharp
+            //var config = Configuration.Default.WithCookies().WithDefaultLoader(s =>
+            //{
+            //    s.IsNavigationEnabled = true;
+            //    s.IsResourceLoadingEnabled = false;
+            //});
+
+            
+        }
+    }
+}
---
+++ StiftungFileImporter/Zefix/ZefixSrv.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.InteropServices;
+using System.ServiceModel.Security;
+using Zefix.ZefixReference;
+
+namespace Zefix
+{
+    public class ZefixSrv
+    {
+        public void FindByName(string name)
+        {
+            var client = new ZefixReference.ZefixServicePortTypeClient();
+            client.ClientCredentials.UserName.UserName = "Name";
+            client.ClientCredentials.UserName.Password = "PW";
+
+            var response = client.SearchByName(new searchByNameRequest {name = name});
+            
+            //var response = client.GetByUidFull(new getByUidRequestType { uid = "110389869" });
+
+        }
+    }
+}
